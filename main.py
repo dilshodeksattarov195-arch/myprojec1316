dccd0b8ad60e9e23bc8eacd957706c4c7a32fd4e
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 21:56:24 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterVncryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterVncryptWorker()
+    print("Worker engine initialized.")
